28b295b79571847b8657ede17aa09bfa9538f5a7
Gaurish Sethia <gaurishsethia@yahoo.com>
2023-10-05 11:57:55 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# elysia-ip
+
+Get the Client Ip Address in Elysia.
+
+It supports Cloudflare, Fastly and other runtimes as well.
+
+## Installation
+
+```bash
+bun a elysia-ip
+```
+
+## Documentation
+
+Please check the wiki section.
+
+## License
+MIT
+
+## Author
+Copyright (c) 2023 Gaurish Sethia, All Rights Reserved.
---
+++ index.ts
@@ -0,0 +1,42 @@
+import { Elysia } from 'elysia';
+
+export type IPHeaders = 'x-real-ip' | 'x-client-ip' | 'cf-connecting-ip' | 'fastly-client-ip' | 'x-cluster-client-ip' | 'x-forwarded' | 'forwarded-for' | 'forwarded' | 'x-forwarded' | 'appengine-user-ip' | 'true-client-ip' | 'cf-pseudo-ipv4' | (string & {})
+
+export const headersToCheck: IPHeaders[] = [
+    'x-real-ip', // Nginx proxy/FastCGI
+    'x-client-ip', // Apache https://httpd.apache.org/docs/2.4/mod/mod_remoteip.html#page-header 
+    'cf-connecting-ip', // Cloudflare
+    'fastly-client-ip', // Fastly
+    'x-cluster-client-ip', // GCP
+    'x-forwarded', // General Forwarded
+    'forwarded-for', // RFC 7239
+    'forwarded', // RFC 7239
+    'x-forwarded', // RFC 7239
+    'appengine-user-ip', // GCP
+    'true-client-ip', // Akamai and Cloudflare
+    'cf-pseudo-ipv4', // Cloudflare 
+]
+
+const getIP = (headers: Headers, checkHeaders: IPHeaders[] = headersToCheck) => {
+    if (typeof checkHeaders === 'string' && headers.get(checkHeaders)) return headers.get(checkHeaders)
+    // X-Forwarded-For is the de-facto standard header
+    if (headers.get('x-forwarded-for')) return headers.get('x-forwarded-for')?.split(',')[0]
+    let clientIP: string | undefined | null = null;
+    if (!checkHeaders) return null;
+    for (const header of checkHeaders) {
+        clientIP = headers.get(header)
+        if (clientIP) break
+    }
+    return clientIP
+}
+
+export const ip = (config: {
+    checkHeaders?: IPHeaders[]
+} = {}) => (app: Elysia) => {
+    return app.derive(({ request }) => {
+        const clientIP = getIP(request.headers, config.checkHeaders)
+        return {
+            ip: clientIP || app.server!.requestIP(request)
+        }
+    })
+}
