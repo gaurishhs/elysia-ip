0f7b489629ed25afe94cb3d2a70ffc39cde0e590
armada45-pixel <armada1@hotmail.co.th>
2024-05-07 13:14:42 +0700

    remove not relevant

--- example/basic.ts
+++ example/basic.ts
@@ -1,10 +1,6 @@
 import { Elysia } from "elysia"
-import { swagger } from "@elysiajs/swagger"
 
 import { ip } from "../src"
-import { Server } from "bun"
-
-let server: Server | null
 
 const app = new Elysia()
   .use(swagger())
@@ -13,5 +9,3 @@ const app = new Elysia()
   .listen(3000, () => {
     console.log("🦊 Swagger is active at: http://localhost:3000/swagger")
   })
-
-server = app.server
