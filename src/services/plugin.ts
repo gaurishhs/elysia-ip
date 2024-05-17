859fb41955c31bef822b65b3e53f880b43f6657a
armada45-pixel <armada1@hotmail.co.th>
2024-05-17 17:04:04 +0700

    using logger edit all message add some condition for check server instace

--- src/services/plugin.ts
+++ src/services/plugin.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia"
 import { getIP } from "./getip"
 import { defaultOptions } from "../constants"
+import { logger } from "./logger"
+
 import type { Options } from "../types"
 
 export const plugin = (userOptions?: Partial<Options>) => (app: Elysia) => {
@@ -13,23 +15,38 @@ export const plugin = (userOptions?: Partial<Options>) => (app: Elysia) => {
     new Elysia({
       name: "elysia-ip",
     }).derive({ as: "global" }, ({ request }): { ip: string } => {
-      if (!options.headersOnly && globalThis.Bun) {
-        const socketAddress = options.injectServer(app)?.requestIP(request)
-        if (socketAddress) return { ip: socketAddress.address }
-        else {
-          console.log(
-            `Elysia server is not initialized. Make sure to call Elyisa.listen()`,
-          )
-          console.log(`use injectServer to inject Server instance`)
+      serverIP: {
+        if (!options.headersOnly && globalThis.Bun) {
+          const server = options.injectServer(app)
+          if (!server) {
+            logger(
+              "plugin",
+              "Elysia server is not initialized. Make sure to call Elyisa.listen()",
+            )
+            logger("plugin", "use injectServer to inject Server instance")
+            break serverIP
+          }
+
+          if (!server.requestIP) {
+            logger("plugin", "server.requestIP is null")
+            logger("plugin", "Please check server instace")
+            break serverIP
+          }
+
+          const socketAddress = server.requestIP(request)
+          logger("plugin", "socketAddress", socketAddress)
+          if (!socketAddress) {
+            logger(
+              "plugin",
+              `ip from server.requestIP return ${typeof socketAddress}`,
+            )
+            break serverIP
+          }
+          return { ip: socketAddress.address }
         }
       }
       return {
-        ip:
-          getIP(request.headers, options.checkHeaders) ||
-          (function () {
-            console.log("Failed to get ip from header!")
-            return ""
-          })(),
+        ip: getIP(request.headers, options.checkHeaders) || "",
       }
     }),
   )
