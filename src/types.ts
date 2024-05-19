372644e166bd05358423ef39a82a119f7ee76c9d
armada45-pixel <armada1@hotmail.co.th>
2024-05-19 12:16:34 +0700

    change duplicate forwarded-for to x-forwarded-for

--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,7 @@ export type IPHeaders =
   | "x-forwarded"
   | "forwarded-for"
   | "forwarded"
-  | "x-forwarded"
+  | "x-forwarded-for"
   | "appengine-user-ip"
   | "true-client-ip"
   | "cf-pseudo-ipv4"
@@ -23,7 +23,7 @@ export interface Options {
    * Customize headers to check for IP address
    * @default ['x-real-ip', 'x-client-ip', 'cf-connecting-ip', 'fastly-client-ip', 'x-cluster-client-ip', 'x-forwarded', 'forwarded-for', 'forwarded', 'x-forwarded', 'appengine-user-ip', 'true-client-ip', 'cf-pseudo-ipv4']
    */
-  checkHeaders?: IPHeaders[]
+  checkHeaders?: IPHeaders | IPHeaders[]
   /**
    * Only check headers regardless of the runtime environment
    * @default false
