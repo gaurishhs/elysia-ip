413e6a05b593c04488a57fdf267f798dcf171814
armada45-pixel <armada1@hotmail.co.th>
2024-05-20 11:37:36 +0700

    resort priority

--- src/constants.ts
+++ src/constants.ts
@@ -1,15 +1,15 @@
 import { Options, IPHeaders } from "./types"
 
 export const headersToCheck: IPHeaders[] = [
-  "x-real-ip", // Nginx proxy/FastCGI
-  "x-client-ip", // Apache https://httpd.apache.org/docs/2.4/mod/mod_remoteip.html#page-header
+  "x-forwarded-for", // RFC 7239
   "cf-connecting-ip", // Cloudflare
   "fastly-client-ip", // Fastly
+  "x-real-ip", // Nginx proxy/FastCGI
+  "x-client-ip", // Apache https://httpd.apache.org/docs/2.4/mod/mod_remoteip.html#page-header
   "x-cluster-client-ip", // GCP
   "x-forwarded", // General Forwarded
   "forwarded-for", // RFC 7239
   "forwarded", // RFC 7239
-  "x-forwarded-for", // RFC 7239
   "appengine-user-ip", // GCP
   "true-client-ip", // Akamai and Cloudflare
   "cf-pseudo-ipv4", // Cloudflare
