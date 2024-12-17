30378c8873ae0648fe643554d4ad91184e9c6c74
Gaurish Sethia <gaurishh.sethia@gmail.com>
2024-12-17 05:06:59 +0000

    Fixes Does not read the IP from `x-forwarded-for` #30

--- src/services/getip.ts
+++ src/services/getip.ts
@@ -6,34 +6,33 @@ export const getIP = (
   headers: Headers,
   checkHeaders: IPHeaders[] = headersToCheck,
 ) => {
+  // User provided single header
   if (typeof checkHeaders === "string" && headers.get(checkHeaders)) {
     debug(`getIP: Found ip from header ${checkHeaders}`);
-    return headers.get(checkHeaders);
+    return headers.get(checkHeaders)
   }
 
-  // X-Forwarded-For is the de-facto standard header
-  if (!checkHeaders && headers.get("x-forwarded-for")) {
-    debug("getIP: IP From Header x-forwarded-for");
-    return headers.get("x-forwarded-for")?.split(",")[0];
+    // check for x-forwaded-for only when user did not provide headers 
+    if (checkHeaders && checkHeaders === headersToCheck && headers.get("x-forwarded-for")) {
+      debug("getIP: IP From Header x-forwarded-for");
+      return headers.get("x-forwarded-for")?.split(",")[0];
+    }
+
+  // User provided / default headers array
+  if (Array.isArray(checkHeaders)) {
+    let clientIP: string | undefined | null = null;
+    for (const header of checkHeaders) {
+      clientIP = headers.get(header);
+      if (clientIP) {
+        debug(`getIP: Found ip from header ${header}`);
+        break;
+      }
+    }
+    return clientIP
   }
 
   if (!checkHeaders) {
     debug("getIP: No checkHeaders");
     return null;
   }
-
-  let clientIP: string | undefined | null = null;
-  for (const header of checkHeaders) {
-    clientIP = headers.get(header);
-    if (clientIP) {
-      debug(`getIP: Found ip from header ${header}`);
-      break;
-    }
-  }
-
-  if (!clientIP) {
-    debug("getIP: Failed to get ip from header!");
-    return;
-  }
-  return clientIP;
 };
